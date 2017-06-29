2a0c2726536c89731bc6af33f8f3c9c0789f0919
Justin Kahrs <justin@justinkahrs.com>
2017-06-28 22:33:06 -0400

    add basic setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Author: Justin Kahrs
+#
+sudo apt-get update
+sudo apt-get install vim git gdebi maven htop -y
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
