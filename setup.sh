2b3c1dedbed72025fd46d8d377e0cd9fe5f75a76
Justin Kahrs <justin@justinkahrs.com>
2017-06-28 22:51:56 -0400

    add atom and intellij

--- setup.sh
+++ setup.sh
@@ -2,7 +2,21 @@
 #
 # Author: Justin Kahrs
 #
+# do an update
 sudo apt-get update
+
+# install these packages
 sudo apt-get install vim git gdebi maven htop -y
+
+# install npm and node via nvm 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+# add umake repo,  install intellij and atom
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
+sudo apt-get update
+sudo apt-get instal ubuntu-make -y
+umake ide idea-ultimate
+umake ide atom
+
+
 
